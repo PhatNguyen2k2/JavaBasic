6760b570e8df6c1fb0f7bc7a6abf2c8626a14cf7
nguye <ngphat1211@gmail.com>
2022-02-09 17:45:00 +0700

    class student

---
+++ first/src/oop/Entry.java
@@ -0,0 +1,28 @@
+package oop;
+
+import java.util.Scanner;
+
+class Student{
+	String name;
+	int age;
+	Student(){
+		name = "";
+		age = 0;
+	}
+	public void input() {
+		Scanner sc = new Scanner(System.in);
+		name = sc.nextLine();
+		age = sc.nextInt();
+		sc.close();
+	}
+	public void output() {
+		System.out.println("Name: " + name + ", Age: " + age);
+	}
+}
+public class Entry {
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		s1.input();
+		s1.output();
+	}
+}
